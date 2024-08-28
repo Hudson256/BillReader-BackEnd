32849e47ca0a44d2623d4d95451a78ab2f5ff5e3
Hudson256 <hudsono256@gmail.com>
2024-08-28 18:32:15 -0300

    chore: adiciona configurações de teste

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/src"],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    testMatch: [
+        "**/__tests__/**/*.test.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[jt]s?(x)"
+    ],
+};
