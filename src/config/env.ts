9c4f791a3768dd1d69b70a5687fb956f9db1c52a
Hudson256 <hudsono256@gmail.com>
2024-08-29 13:25:05 -0300

    fix(config): Define links ausentes no arquivo env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    geminiApiUrl: process.env.GEMINI_API_URL || 'http:
+    geminiApiUrl: process.env.GEMINI_API_URL || 'http://localhost:3000',
     port: process.env.PORT || 3000,
 };
